143af7b1cf565c96272bb8dd37836fc79abb813a
ludwings <ludwings0330@gmail.com>
2022-02-13 22:10:37 +0900

    [Java] generics 이해와 사용법

---
+++ src/main/java/genericsstudy/GenericExample.java
@@ -0,0 +1,34 @@
+package genericsstudy;
+
+public class GenericExample {
+    static class Box<T> {
+        T item;
+        void setItem(T item) {
+            this.item = item;
+        }
+        
+        T getItem() {
+            return this.item;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        FruitBox<Apple> appleBox = new FruitBox<Apple>();
+
+
+        Box<String> box = new Box<String>();
+        box.setItem("ABC");
+        String item = box.getItem(); // (String)으로 형변환이 필요없음 컴파일시에 타입 체크
+        System.out.println("item = " + item);
+    }
+
+    private static class Apple {
+    }
+
+    private static class FruitBox<T extends Fruit> { // Fruit의 자손만 타입으로 지정가능
+    }
+
+    private static class Fruit {
+    }
+}
